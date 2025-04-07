2e1ef4d14977b82a41216a2588ab000ceb176dcb
allisonopix <katakiriamoto0@gmail.com>
2025-04-07 17:02:17 -0300

    Add files via upload

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,17 +1,11 @@
 import style from './menu.module.css'
 
-
 export const Menu = (props) => {
     return(
-        <>
-        <nav className={style.navbar}>
-            <p><a href="#s1">{props.option01}</a></p>
-            <p><a href="#s2">{props.option02}</a></p>
-            <p><a href="#s3">{props.option03}</a></p>
-            <p><a href="#s4">{props.option04}</a></p>
-            <p><a href={'.Contact'}>{props.option05}</a></p>
-        </nav>
-        </>
+            <nav className={style.navBar}>
+                <p><a href="#s1">{props.option01}</a></p>
+                <p><a href="#s2">{props.option02}</a></p>
+                <p><a href={'/contact'}>{props.option03}</a></p>
+            </nav>
     )
 }
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
 import './index.css'
 import App from './App.jsx'
+import Contact from './contact.jsx'
+
+import { Analytics } from "@vercel/analytics/react"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <Analytics/>
+     <RouterProvider router={router} />
   </StrictMode>,
 )
